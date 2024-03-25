73a201c277cadba3ac9d2e2a7166aad7b9dfb46e
delvan <anikeev30@yandex.ru>
2024-03-25 20:22:52 +0900

    изменено:   brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -21,7 +21,7 @@ def play_game():
             print("Correct!")
             correct_answers += 1
         else:
-            print(f"'{user_answer}' is wrong answer ;(."
+            print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{'yes' if is_even(number) else 'no'}'.")
             print(f"Let's try again, {name}!")
             return
