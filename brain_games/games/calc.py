b863b46a2fc01078870fe741550b12c4655b7a9d
delvan <anikeev30@yandex.ru>
2024-03-27 00:23:02 +0900

    изменено:      brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,45 +2,37 @@ from brain_games.cli import welcome_user
 import random
 
 
-def calculate(num1, num2, operator):
-    if operator == "+":
-        return num1 + num2
-    elif operator == "-":
-        return num1 - num2
-    elif operator == "*":
-        return num1 * num2
+def calculate(expression):
+    return eval(expression)
 
 
 def calc_game():
-    print("\nbrain-calc\n")
+    print("brain-calc\n")
     name = welcome_user()
-    print("\nWhat is the result of the expression?")
+    print("What is the result of the expression?")
 
-    max_rounds = 3
     correct_answers = 0
-
-    for _ in range(max_rounds):
-        num1 = random.randint(1, 100)
-        num2 = random.randint(1, 100)
+    for _ in range(3):
+        num1 = random.randint(1, 50)
+        num2 = random.randint(1, 50)
         operator = random.choice(["+", "-", "*"])
         expression = f"{num1} {operator} {num2}"
 
         print(f"Question: {expression}")
-        user_answer = int(input("Your answer: "))
+        user_answer = input("Your answer: ")
 
-        correct_answer = calculate(num1, num2, operator)
-        if user_answer == correct_answer:
+        correct_answer = calculate(expression)
+        if int(user_answer) == correct_answer:
             print("Correct!")
             correct_answers += 1
         else:
-            print(
-                f"'{user_answer}' is the wrong answer ;(. "
-                f"The correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
+                  f"'{correct_answer}'.")
             print(f"Let's try again, {name}!")
-            break
+            return
 
-    if correct_answers == max_rounds:
-        print(f"Congratulations, {name}!")
+    print(f"Congratulations, {name}!")
+    return
 
 
 calc_game()
