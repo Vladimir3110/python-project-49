aab0aa92d850076d305927db6f54e6306f8468a4
delvan <anikeev30@yandex.ru>
2024-03-27 13:26:22 +0900

    изменено:      brain_games/games/calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,37 +2,31 @@ from brain_games.cli import welcome_user
 import random
 
 
-def calculate(expression):
-    return eval(expression)
-
-
 def calc_game():
     print("brain-calc\n")
     name = welcome_user()
-    print("What is the result of the expression?")
 
-    correct_answers = 0
-    for _ in range(3):
-        num1 = random.randint(1, 50)
-        num2 = random.randint(1, 50)
+    count = 0
+    while count < 3:
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
         operator = random.choice(["+", "-", "*"])
         expression = f"{num1} {operator} {num2}"
+        correct_answer = eval(expression)
 
         print(f"Question: {expression}")
-        user_answer = input("Your answer: ")
+        user_answer = int(input("Your answer: "))
 
-        correct_answer = calculate(expression)
-        if int(user_answer) == correct_answer:
-            print("Correct!")
-            correct_answers += 1
+        if user_answer == correct_answer:
+            print("Correct!\n")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
-                  f"'{correct_answer}'.")
-            print(f"Let's try again, {name}!")
+            print(f"{user_answer} - wrong answer ;(. The correct answer was "
+                  f" {correct_answer}.\nLet's try again, {name}!\n")
             return
 
+        count += 1
+
     print(f"Congratulations, {name}!")
-    return
 
 
 calc_game()
