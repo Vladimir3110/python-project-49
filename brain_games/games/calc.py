acc78ba8326ccc47a39709dec411ed5a7d8b7b9b
Владимир <160821564+Vladimir3110@users.noreply.github.com>
2024-03-27 18:23:04 +1000

    Update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,23 +7,25 @@ def calc_game():
     name = welcome_user()
     print("What is the result of the expression?")
 
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
-    operator = random.choice(["+", "-", "*"])
-    expression = f"{num1} {operator} {num2}"
-    correct_answer = eval(expression)
+    correct_answers_count = 0
+    while correct_answers_count < 3:
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        operator = random.choice(["+", "-", "*"])
+        expression = f"{num1} {operator} {num2}"
+        correct_answer = eval(expression)
 
-    print(f"Question: {expression}")
-    user_answer = int(input("Your answer: "))
+        print(f"Question: {expression}")
+        user_answer = int(input("Your answer: "))
 
-    if user_answer == correct_answer:
-        print("Correct!\n")
-    else:
-        print(f"'{user_answer}' is the wrong answer ;(. Correct answer was "
-              f"'{correct_answer}'.")
-        print(f"Let's try again, {name}!")
-
-    print(f"Congratulations, {name}!")
+        if user_answer == correct_answer:
+            print("Correct!\n")
+            correct_answers_count += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
+                  f"'{correct_answer}'.")
+            print(f"Let's try again, {name}!")
+            return
 
 
 calc_game()
