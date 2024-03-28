755c9118c2b348a80c925b1c695e8a8974df5f89
delvan <anikeev30@yandex.ru>
2024-03-28 21:50:41 +0900

    добавлена команда в brain_games/constants.py

--- brain_games/constants.py
+++ brain_games/constants.py
@@ -1,3 +1,4 @@
 GAME_INSTRUCTIONS = {
     "even": '''Answer "yes" if the number is even, otherwise answer "no".''',
+    "prime": 'Answer "yes" if given number is prime. Otherwise answer "no".',
 }
