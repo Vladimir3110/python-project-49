eeec98a5c67095accc6ddad4786a76d5ce46db15
delvan <anikeev30@yandex.ru>
2024-03-28 21:52:24 +0900

    изменено:  brain_games/games/prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,12 +1,13 @@
 from brain_games.cli import welcome_user
+from brain_games.constants import GAME_INSTRUCTIONS
 import random
-import math
+import prompt
 
 
 def is_prime(number):
     if number < 2:
         return False
-    for i in range(2, int(math.sqrt(number)) + 1):
+    for i in range(2, number):
         if number % i == 0:
             return False
     return True
@@ -15,26 +16,20 @@ def is_prime(number):
 def play_prime():
     print("brain-prime\n")
     name = welcome_user()
-    print("'yes' if given number is prime. Otherwise answer 'no''.")
+    print(GAME_INSTRUCTIONS["prime"])
 
     for _ in range(3):
-        question = random.randint(1, 100)
-        correct_answer = "yes" if is_prime(question) else "no"
-
-        print(f"Question: {question}")
-        user_answer = input("Your answer: ")
+        number = random.randint(1, 100)
+        print(f"Question: {number}")
+        user_answer = prompt.string("Your answer: ")
+        correct_answer = "yes" if is_prime(number) else "no"
 
         if user_answer == correct_answer:
             print("Correct!")
         else:
-            print(f""
-                  f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was "
+                  f"'{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
     print(f"Congratulations, {name}!")
-
-
-if __name__ == "__main__":
-    play_prime()
