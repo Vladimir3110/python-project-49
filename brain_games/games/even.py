736eaaa59b29c0f96221a6317fd3f9bae91b08af
delvan <anikeev30@yandex.ru>
2024-04-07 22:50:59 +0900

    изменено:  brain_games/games/even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,39 +1,17 @@
-from brain_games.cli import welcome_user
+from brain_games.utils import generate_rand_num
+from brain_games.the_engine import run_game
 from brain_games.constants import GAME_INSTRUCTIONS
-import random
 
 
-def is_even(number):
-    return number % 2 == 0
+def is_even(num):
+    return num % 2 == 0
 
 
-def generate_question():
-    number = random.randint(1, 100)
-    return str(number)
-
-
-def get_correct_answer(number):
-    if is_even(int(number)):
-        return 'yes'
-    return 'no'
+def get_num_and_even_answer():
+    num = generate_rand_num()
+    correct_answer = 'yes' if is_even(num) else 'no'
+    return num, correct_answer
 
 
 def play_game():
-    name = welcome_user()
-    print(GAME_INSTRUCTIONS["even"])
-
-    for _ in range(3):
-        question = generate_question()
-        print("Question: " + question)
-        user_answer = input("Your answer: ")
-        correct_answer = get_correct_answer(question)
-
-        if user_answer.lower() == correct_answer:
-            print("Correct!")
-        else:
-            print("'" + user_answer + "' is wrong answer ;(. Correct answer was"
-                                      " '" + correct_answer + "'.")
-            print("Let's try again, " + name + "!")
-            return
-
-    print("Congratulations, " + name + "!")
+    run_game(get_num_and_even_answer, GAME_INSTRUCTIONS["even"])
