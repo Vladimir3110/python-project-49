a59a26050140b072fff0385850eb371d5612fdcf
delvan <anikeev30@yandex.ru>
2024-04-07 22:54:15 +0900

    изменено:  brain_games/games/gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,36 +1,20 @@
-from brain_games.cli import welcome_user
-import random
+from brain_games.utils import generate_rand_num
 import math
+from brain_games.the_engine import run_game
+from brain_games.constants import GAME_INSTRUCTIONS
 
 
-def generate_question():
-    num1 = random.randint(1, 10)
-    num2 = random.randint(1, 10)
-    question = f"Question: {num1} {num2}"
-    return question, math.gcd(num1, num2)
+def get_gcd(first_num, second_num):
+    return math.gcd(first_num, second_num)
+
+
+def get_number_pair_and_gcd():
+    first_num, second_num = generate_rand_num(10), generate_rand_num(10)
+    gcd = get_gcd(first_num, second_num)
+    nums = f'{first_num} {second_num}'
+
+    return nums, str(gcd)
 
 
 def gcd_game():
-    print("brain-gcd")
-    name = welcome_user()
-    print("Find the greatest common divisor of given numbers.")
-
-    correct_answers = 0
-    while correct_answers < 3:
-        question, correct_answer = generate_question()
-        print(question)
-        user_answer = int(input("Your answer: "))
-        if user_answer == correct_answer:
-            print("Correct!")
-            correct_answers += 1
-        else:
-            print(f""
-                  f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}!")
-            return
-        print(f"Congratulations, {name}!")
-
-
-if __name__ == '__main__':
-    gcd_game()
+    run_game(get_number_pair_and_gcd, GAME_INSTRUCTIONS["gcd"])
